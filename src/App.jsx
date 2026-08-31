1f4acbb64d4bf9a46e8628d3ead0d25fb2735f01
NenoPr <nenadpredan@gmail.com>
2026-08-31 19:04:57 +0200

    Added feature to sort the movies through decades, no longer can you have duplicates in watchlist, no longer will you be able to add movie to watchlist if you have already added it, did some minor css design on buttons, made other bug fixes.

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
   const [movies, setMovies] = useState([]);
   const [watchlist, setWatchlist] = useState([]);
   const [toggleWatchlist, setToggleWatchlist] = useState(false);
+  const [toggletime, setToggleTime] = useState("all-years");
 
   const handleSearch = async () => {
     const url = `https://api.themoviedb.org/3/search/movie?query=${encodeURIComponent(query)}`;
@@ -36,11 +37,40 @@ function App() {
   }, [watchlist]);
 
   const addToWatchlist = (movie) => {
-    setWatchlist((prewatchlist) => [
-      ...prewatchlist,
-      { ...movie, watched: false, user_rating: 0 },
-    ]);
+    setWatchlist((prev) =>
+      prev.some((item) => item.id === movie.id)
+        ? prev
+        : [...prev, { ...movie, watched: false, user_rating: 0 }],
+    );
   };
+
+  const filteredTime = movies.filter((movie) => {
+    if (toggletime === "all-years") {
+      return movie;
+    } else if (toggletime === "2020+") {
+      if (movie.release_date.slice(0, 4) > 2020) {
+        return movie;
+      }
+    } else if (toggletime === "2010+") {
+      if (
+        movie.release_date.slice(0, 4) < 2020 &&
+        movie.release_date.slice(0, 4) > 2009
+      ) {
+        return movie;
+      }
+    } else if (toggletime === "2000+") {
+      if (
+        movie.release_date.slice(0, 4) < 2010 &&
+        movie.release_date.slice(0, 4) > 1999
+      ) {
+        return movie;
+      }
+    } else if (toggletime === "before-2000") {
+      if (movie.release_date.slice(0, 4) < 2000) {
+        return movie;
+      }
+    }
+  });
 
   return (
     <div className="main">
@@ -54,42 +84,71 @@ function App() {
 
         <button onClick={handleSearch}>Search</button>
       </div>
-      <div
+      <button
         className="watchlist-button"
         onClick={() => setToggleWatchlist(!toggleWatchlist)}
       >
-        My Watchlist
-      </div>
+        {toggleWatchlist ? "<-- Search Results" : "My Watchlist -->"}
+      </button>
 
       {toggleWatchlist ? (
         <Watchlist watchlist={watchlist} setWatchlist={setWatchlist} />
       ) : (
-        <div className="movies-card-holder">
-          {movies.map((movie) => (
-            <div key={movie.id} className="movie-card">
-              <div className="movie-poster">
-                <img
-                  className="poster-image"
-                  src={
-                    movie.poster_path
-                      ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
-                      : "/no_poster.png"
-                  }
-                  alt={movie.title}
-                />
+        <>
+          <div className="age-range-container">
+            <button
+              className="all-years"
+              onClick={() => setToggleTime("all-years")}
+            >
+              All Years
+            </button>
+            <button className="2020+" onClick={() => setToggleTime("2020+")}>
+              2020+
+            </button>
+            <button className="2010+" onClick={() => setToggleTime("2010+")}>
+              2010+
+            </button>
+            <button className="2000+" onClick={() => setToggleTime("2000+")}>
+              2000+
+            </button>
+            <button
+              className="before-2000"
+              onClick={() => setToggleTime("before-2000")}
+            >
+              Before 2000
+            </button>
+          </div>
+          <div className="movies-card-holder">
+            {filteredTime.map((movie) => (
+              <div key={movie.id} className="movie-card">
+                <div className="movie-poster">
+                  <img
+                    className="poster-image"
+                    src={
+                      movie.poster_path
+                        ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
+                        : "/no_poster.png"
+                    }
+                    alt={movie.title}
+                  />
+                </div>
+                <div className="movie-details">
+                  <h2>{movie.title}</h2>
+                  <p>Release Date: {movie.release_date}</p>
+                  <p>Rating: {movie.vote_average}</p>
+                  <p>{movie.overview}</p>
+                  {watchlist.some((item) => item.id === movie.id) ? (
+                    <button disabled>Added</button>
+                  ) : (
+                    <button onClick={() => addToWatchlist(movie)}>
+                      Add to Watchlist
+                    </button>
+                  )}
+                </div>
               </div>
-              <div className="movie-details">
-                <h2>{movie.title}</h2>
-                <p>Release Date: {movie.release_date}</p>
-                <p>Rating: {movie.vote_average}</p>
-                <p>{movie.overview}</p>
-                <button onClick={() => addToWatchlist(movie)}>
-                  Add to Watchlist
-                </button>
-              </div>
-            </div>
-          ))}
-        </div>
+            ))}
+          </div>
+        </>
       )}
     </div>
   );
--- src/components/watchlist.jsx
+++ src/components/watchlist.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import "../App.css";
 
 function Watchlist({ watchlist, setWatchlist }) {
+  const [currentTab, setCurrentTab] = useState("all");
   useEffect(() => {
     console.log(watchlist);
   }, []);
@@ -24,65 +25,98 @@ function Watchlist({ watchlist, setWatchlist }) {
       ),
     );
   };
+  const filteredWatchlist = watchlist.filter((movie) => {
+    if (currentTab === "watched") {
+      return movie.watched === true;
+    }
+
+    if (currentTab === "not-watched") {
+      return movie.watched === false;
+    }
+
+    return true;
+  });
   return (
-    <div className="movies-card-holder">
-      {watchlist.map((movie) => {
-        return (
-          <div key={movie.id} className="movie-card">
-            <div className="movie-poster">
-              <img
-                className="poster-image"
-                src={
-                  movie.poster_path
-                    ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
-                    : "/no_poster.png"
-                }
-                alt={movie.title}
-              />
-            </div>
-            <div className="movie-details">
-              <h2>{movie.title}</h2>
-              <p>Release Date: {movie.release_date}</p>
-              <div>
-                <p>
-                  Click to set show as{" "}
-                  {movie.watched ? "not watched:" : "as watched:"}
-                </p>
-                <button onClick={() => updateWatchlistWatched(movie.id)}>
-                  {movie.watched ? "Completed" : "Not watched"}
+    <>
+      <div className="watched-tabs">
+        <button
+          className={`all-watchlist watchlist-tab ${currentTab === "all" ? "watchlist-tab-selected" : ""}`}
+          onClick={() => setCurrentTab("all")}
+        >
+          All
+        </button>
+        <button
+          className={`watched-watchlist watchlist-tab ${currentTab === "watched" ? "watchlist-tab-selected" : ""}`}
+          onClick={() => setCurrentTab("watched")}
+        >
+          Watched
+        </button>
+        <button
+          className={`not-watched-watchlist watchlist-tab ${currentTab === "not-watched" ? "watchlist-tab-selected" : ""}`}
+          onClick={() => setCurrentTab("not-watched")}
+        >
+          Not Watched
+        </button>
+      </div>
+      <div className="movies-card-holder">
+        {filteredWatchlist.map((movie) => {
+          return (
+            <div key={movie.id} className="movie-card">
+              <div className="movie-poster">
+                <img
+                  className="poster-image"
+                  src={
+                    movie.poster_path
+                      ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
+                      : "/no_poster.png"
+                  }
+                  alt={movie.title}
+                />
+              </div>
+              <div className="movie-details">
+                <h2>{movie.title}</h2>
+                <p>Release Date: {movie.release_date}</p>
+                <div>
+                  <p>
+                    Click to set show as{" "}
+                    {movie.watched ? "not watched:" : "as watched:"}
+                  </p>
+                  <button onClick={() => updateWatchlistWatched(movie.id)}>
+                    {movie.watched ? "Completed" : "Not watched"}
+                  </button>
+                </div>
+                <div>change your rating:</div>
+                <p>Your Rating:</p>
+                <select
+                  name="rating"
+                  id="rating"
+                  value={movie.user_rating}
+                  onChange={(event) =>
+                    updateWatchlistRating(movie.id, event.target.value)
+                  }
+                >
+                  <option value="0">Not rated</option>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
+                  <option value="6">6</option>
+                  <option value="7">7</option>
+                  <option value="8">8</option>
+                  <option value="9">9</option>
+                  <option value="10">10</option>
+                </select>
+                <p></p>
+                <button onClick={() => removeWatchlist(movie.id)}>
+                  Remove from watchlist
                 </button>
               </div>
-              <div>change your rating:</div>
-              <p>Your Rating:</p>
-              <select
-                name="rating"
-                id="rating"
-                value={movie.user_rating}
-                onChange={(event) =>
-                  updateWatchlistRating(movie.id, event.target.value)
-                }
-              >
-                <option value="0">Not rated</option>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
-                <option value="8">8</option>
-                <option value="9">9</option>
-                <option value="10">10</option>
-              </select>
-              <p></p>
-              <button onClick={() => removeWatchlist(movie.id)}>
-                Remove from watchlist
-              </button>
             </div>
-          </div>
-        );
-      })}
-    </div>
+          );
+        })}
+      </div>
+    </>
   );
 }
 
